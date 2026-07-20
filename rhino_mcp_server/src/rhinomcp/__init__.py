dedb2e375a4197c15d84b72850c56cfe1d1d1b54
johanesmikhael <johanes.mikhael@gmail.com>
2026-07-20 16:07:45 +0200

    Bump version to 0.2.4

--- rhino_mcp_server/src/rhinomcp/__init__.py
+++ rhino_mcp_server/src/rhinomcp/__init__.py
@@ -1,6 +1,6 @@
 """Rhino integration through the Model Context Protocol."""
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 # Expose key classes and functions for easier imports
 from .static.rhinoscriptsyntax import rhinoscriptsyntax_json
